84b393a08d79edac3da498153fb8548deef3b784
TNHacker01 <81187838+TNHacker01@users.noreply.github.com>
2023-07-24 16:28:32 +0100

    Add files via upload

---
+++ Second/js/typeWriter.js
@@ -0,0 +1,36 @@
+
+let i = 0;
+let text1 = "Aya wenek chna7welk hhh";
+let text2 = "7abbit n9ollek happy birthday 7abibi w nchallah b toul la3mor w n7ebbk brcha brcha brcha,mouaahh"
+let speed = 50;
+
+function typeWriter(text, para){
+	if(ok == 2){
+		clearInterval(typeInterval);
+	}
+	if(i < text.length){
+		document.getElementById(para).innerHTML += text.charAt(i);
+		i++;
+		speed = Math.random() * 30 + 100;
+	}
+	else{
+		if(ok == 0){
+			i = 0;
+		}
+		ok += 1;
+	}
+}
+
+var typeInterval;
+
+//window.onload = function() {
+//	window.onload = function(){};
+   	typeInterval = setInterval(function(){
+		if(ok == 0){
+			typeWriter(text1, "txt1");
+		}
+		else if(ok == 1){
+			typeWriter(text2, "txt2");
+		}
+	}, 100);
+//};
